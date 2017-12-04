f23754b05a14900429c8e83c976d2e8dbc105037
堂福大成 <>
2017-12-04 20:43:26 +0900

    Add routes for group

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "top#index"
   devise_for :users
   resources :users, only: [:show, :edit, :update]
+  resources :groups, only: [:show, :edit, :update]
 
 end
