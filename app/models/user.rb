d6e0ba2a084c330c7f3cf4fbca4d3c5dfc526a10
堂福大成 <>
2017-12-04 19:28:16 +0900

    Modify show view and create name action and name_kana action

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,28 +6,37 @@ class User < ApplicationRecord
          authentication_keys: [:email, :group_key]
   attr_accessor :group_key
 
-#association
-belongs_to :group
+  #association
+  belongs_to :group
 
-#validation
-before_validation :group_key_to_id, if: :has_group_key?
+  #validation
+  before_validation :group_key_to_id, if: :has_group_key?
 
-def self.find_first_by_auth_conditions(warden_conditions)
-conditions = warden_conditions.dup
-group_key = conditions.delete(:group_key)
-group_id = Group.where(key: group_key).first
-email = conditions.delete(:email)
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    group_key = conditions.delete(:group_key)
+    group_id = Group.where(key: group_key).first
+    email = conditions.delete(:email)
 
-# devise認証を、複数項目に対応させる
-if group_id && email
-where(conditions).where(["group_id = :group_id AND email = :email",
- { group_id: group_id, email: email }]).first
-elsif conditions.has_key?(:confirmation_token)
-where(conditions).first
-else
-false
-end
-end
+    # devise認証を、複数項目に対応させる
+    if group_id && email
+      where(conditions).where(["group_id = :group_id AND email = :email",
+      { group_id: group_id, email: email }]).first
+    elsif conditions.has_key?(:confirmation_token)
+      where(conditions).first
+    else
+      false
+    end
+  end
+
+
+  def name
+    "#{family_name} #{first_name}"
+  end
+
+  def name_kana
+    "#{family_name_kana} #{first_name_kana}"
+  end
 
 private
 def has_group_key?
