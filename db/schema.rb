d23d2cc2cdf67dc25a4f4feacd615a30a9a027d3
堂福大成 <>
2017-12-04 18:00:46 +0900

    Add group_id to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204085049) do
+ActiveRecord::Schema.define(version: 20171204085701) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "key"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20171204085049) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "group_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
