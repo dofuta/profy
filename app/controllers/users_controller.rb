9a5e171ea97ea0df4beb61ffa8cb16b887a94e02
堂福大成 <>
2017-12-10 15:48:11 +0900

    Add edit button and func in question view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,5 @@ class UsersController < ApplicationController
   def update_params
     params.require(:user).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :avatar)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   root "top#index"
   resources :groups, only: [:show, :edit, :update]
   resources :questions, only: [:create, :show]
-  resources :answers, only: [:new, :create]
+  resources :answers, only: [:new, :create, :edit, :update]
 end
