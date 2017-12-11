6e84c4684d6ba6a849b7962dc598b4e8e9069267
堂福大成 <>
2017-12-11 17:37:42 +0900

    Change timeline as created_at DESC

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,5 +2,7 @@ class TopController < ApplicationController
   def index
     @question = Question.new
     @questions = current_user.group.questions
+    @feed_contents = current_user.group.feed_contents.includes(:content).page(params[:page]).per(10)
+    @feed_contents_resource = @feed_contents.map(&:content)
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,22 +2,26 @@ class Question < ApplicationRecord
   #association
   belongs_to :group
   belongs_to :user
-  has_many :answers
-  has_many :feed_content, as: :content, dependent: :destroy
+  has_many :answers, dependent: :destroy
+  has_one :feed_content, as: :content, dependent: :destroy
 
   #validation
   validates_presence_of :user_id, :text, :group_id
 
   #callback
-after_create :create_feed_content
+  after_create :create_feed_content
 
   def user_answer(user_id)
     Answer.find_by(user_id: user_id, question_id: id)
   end
 
+  def answered?(user)
+    self.answers.exists?(user_id: user.id)
+  end
+
   private
   def create_feed_content
-    self.feed_content = FeedContent.create(group_id: group_id, updated_at: updated_at)
+    self.feed_content = FeedContent.create(group_id: self.group_id, updated_at: self.updated_at)
   end
 
 end
